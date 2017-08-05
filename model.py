2f928c65079ca8c0f69b6bd8f0392e1d0ce1f8d4
Maximilian Wenger <mwenger@uos.de>
2017-08-05 21:19:59 +0200

    Add LeNet

--- model.py
+++ model.py
@@ -33,13 +33,32 @@ y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Flatten())
-model.add(Dense(1))
+def simple_model():
+	model = Sequential()
+	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+	model.add(Flatten())
+	model.add(Dense(1))
+	return model
+
+def le_net():
+	model = Sequential()
+	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+	model.add(Convolution2D(6,5,5,activation="relu"))
+	model.add(MaxPooling2D())
+	model.add(Convolution2D(6,5,5,activation="relu"))
+	model.add(MaxPooling2D())
+	model.add(Flatten())
+	model.add(Dense(120))
+	model.add(Dense(84))
+	model.add(Dense(1))
+	return model
+
+model = le_net()
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
+exit()
