2d46b9c147ae18284b7383027390e415801a42e3
Maximilian Wenger <mwenger@uos.de>
2017-08-06 15:07:25 +0200

    Cleanup

--- model.py
+++ model.py
@@ -5,33 +5,11 @@ import cv2
 import os
 import time
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
-def simple_model():
-	model = Sequential()
-	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-	model.add(Flatten())
-	model.add(Dense(1))
-	return model
-
-def le_net(cropping = False):
-	model = Sequential()
-	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-	if cropping:
-		model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-	model.add(Convolution2D(6,5,5,activation="relu"))
-	model.add(MaxPooling2D())
-	model.add(Convolution2D(6,5,5,activation="relu"))
-	model.add(MaxPooling2D())
-	model.add(Flatten())
-	model.add(Dense(120))
-	model.add(Dense(84))
-	model.add(Dense(1))
-	return model
-
 def nvidia(cropping=True):
-	keep_prob = 1.0
+	"""Create a Nvidia convolutional network with reduced number of convolutional layers."""
 	model = Sequential()
 	model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 	if cropping:
@@ -47,36 +25,29 @@ def nvidia(cropping=True):
 	model.add(Convolution2D(64//2,kernel_size=(1,1),activation="relu",padding='valid'))
 	model.add(Flatten())
 	model.add(Dense(100))
-	model.add(Dropout(keep_prob))
 	model.add(Dense(50))
-	model.add(Dropout(keep_prob))
 	model.add(Dense(10))
 	model.add(Dense(1))
 	return model
 
-#model = le_net(False)
 model = nvidia()
 model.summary()
 
-def should_use_measurement():
+def use_measurement_anyway():
 	return random.randint(0, 2) < 1 # use a third of measurements
 
-#data_dir = "/home/mw/data"
-#data_dir = "/home/mw/training2"
-
-steering_correction = 1.3
 data_dir_prefix = "/home/mw/p3"
 data_dirs = ["data",
 			 "fix1",
 			 "fix2",
 			 "fix3",
-			 #"recovery",
 			]
 
 images = []
 measurements = []
 previous_length = 0
 
+# Load images and measurements from all directories in data_dirs
 from keras.preprocessing.image import img_to_array, load_img
 for current_dir in data_dirs:
 	data_dir = os.path.join(data_dir_prefix, current_dir)
@@ -92,32 +63,16 @@ for current_dir in data_dirs:
 	for line in lines:
 		source_path = line[0]
 		filename = source_path.split('/')[-1]
-		#image_left = cv2.imread(os.path.join(data_dir, "IMG", line[1].split('/')[-1]))
-		#image_right = cv2.imread(os.path.join(data_dir, "IMG", line[2].split('/')[-1]))
 		current_path = os.path.join(data_dir, "IMG", filename)
-		#image = cv2.imread(current_path)
 		image = load_img(current_path)
 		image = img_to_array(image)
-		#print(image)
 		measurement = float(line[3])
-		measurement_left = measurement * steering_correction
-		measurement_right = -measurement * steering_correction
-		if measurement != 0.0 or should_use_measurement():
+		if measurement != 0.0 or use_measurement_anyway(): # don't use all of the zero steering data
 			measurements.append(measurement)
 			images.append(image)
 			# add flipped images
-			#images.append(cv2.flip(image,1))
 			images.append(np.fliplr(image))
 			measurements.append(measurement*-1.0)
-			# add left/right images
-		#	measurements.append(measurement_left)
-		#	images.append(image_left)
-		#	measurements.append(measurement_right)
-		#	images.append(image_right)
-		#	measurements.append(-1.0*measurement_left)
-		#	images.append(cv2.flip(image_left,1))
-		#	measurements.append(-1.0*measurement_right)
-		#	images.append(cv2.flip(image_right,1))
 		else:
 			skipped += 1
 
@@ -128,12 +83,16 @@ for current_dir in data_dirs:
 		print("Skipped {} images with steering of zero.".format(skipped))
 print("\n\n")
 
+# Set up the training data
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 
+# Train the model
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
+# Save the model
 model.save('model.h5')
+
 exit()
